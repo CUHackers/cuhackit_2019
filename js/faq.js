27b3aa0ccd47a8870620f2db6d9f61782051f916
Charlie Gallentine <chgallentine@gmail.com>
2018-01-15 14:38:36 -0500

    Format shtuff

--- js/faq.js
+++ js/faq.js
@@ -1,13 +1,9 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+const accordionTitle = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
+for (let i = 0; i < accordionTitle.length; i++) {
+    accordionTitle[i].addEventListener("click", function() {
         this.classList.toggle("active");
 
-        /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
