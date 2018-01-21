9d80afa45f8bb91668122c6bf23e45efe23a4f6b
Charlie Gallentine <chgallentine@gmail.com>
2018-01-21 15:57:36 -0500

    Troubleshooting my misunderstandings

--- js/faq.js
+++ js/faq.js
@@ -4,11 +4,11 @@ for (let i = 0; i < accordionTitle.length; i++) {
   	accordionTitle[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    
+
     if (panel.style.maxHeight) {
     	panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = 200px;
     }
   });
 }
