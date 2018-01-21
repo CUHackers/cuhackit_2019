876f57373db75bc6d25f0ebf1819c99ca12955b1
Charlie Gallentine <chgallentine@gmail.com>
2018-01-21 15:54:56 -0500

    i broke a thing

--- js/faq.js
+++ js/faq.js
@@ -3,8 +3,8 @@ const accordionTitle = document.getElementsByClassName("accordion");
 for (let i = 0; i < accordionTitle.length; i++) {
   	accordionTitle[i].addEventListener("click", function() {
     this.classList.toggle("active");
-
     var panel = this.nextElementSibling;
+    
     if (panel.style.maxHeight) {
     	panel.style.maxHeight = null;
     } else {
