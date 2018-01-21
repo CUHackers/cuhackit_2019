b70d29d56713ab413417916fe5e231f9f8d59e8e
Charlie Gallentine <chgallentine@gmail.com>
2018-01-21 15:48:37 -0500

    Added smooth transition to faqs

--- js/faq.js
+++ js/faq.js
@@ -1,14 +1,14 @@
 const accordionTitle = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < accordionTitle.length; i++) {
-  accordionTitle[i].addEventListener("click", function() {
+  	accordionTitle[i].addEventListener("click", function() {
     this.classList.toggle("active");
 
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+    	panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";  
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
