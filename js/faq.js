f9705c3271addae48f92fbb727ba3eab2eb0a975
Charlie Gallentine <chgallentine@gmail.com>
2018-01-21 20:55:08 -0500

    Added code so only one panel at a time is active

--- js/faq.js
+++ js/faq.js
@@ -2,13 +2,20 @@ const accordionTitle = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < accordionTitle.length; i++) {
   	accordionTitle[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+  		let panel = this.nextElementSibling;
 
-    if (panel.style.maxHeight) {
-    	panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+  		for (let j = 0; j < accordionTitle.length; j++) {
+  			if (panel.style.maxHeight) {
+  				panel.style.maxHeight = null;
+  			}
+  		}
+  		
+    	this.classList.toggle("active");
+
+    	if (panel.style.maxHeight) {
+    		panel.style.maxHeight = null;
+    	} else {
+     		panel.style.maxHeight = panel.scrollHeight + "px";
+    	}
   });
 }
