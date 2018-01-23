e4bb8714e737b5c98e304f4d24043d6157d816e0
Angelo Carrabba <acarrabba6@gmail.com>
2018-01-23 15:12:50 -0500

    updated the css and stuff

--- js/register.js
+++
@@ -1,8 +0,0 @@
-const registerLink = 'https://docs.google.com/forms/d/e/1FAIpQLSe74iE8SkqVFOAR9szMRofQZZuvF_ptVntQmUlIxPupSjg4Fg/viewform?usp=sf_link';
-
-$(document).ready(() => {
-    $('.register').click(event => {
-        window.open(registerLink, '_blank');
-    });
-});
-
