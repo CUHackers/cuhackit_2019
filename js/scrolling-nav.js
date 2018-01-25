bdf8917149c99414df05f08e55c64beb849a7744
Angelo Carrabba <acarrabba6@gmail.com>
2018-01-25 18:51:02 -0500

    updated scrollspy to hide apply button when other one is showing.

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,29 +1,42 @@
-(function($) {
-  "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 52)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: "#myNavbar", offset: 100});
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#myNavbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
 
-})(jQuery); // End of use strict
+    function activateOnScroll() {
+	var x = $('#myNavbar  a.active').attr('href');
+	console.log("active: " + x);
+	if (x === "#top") {
+	    $('#hideOnCountdown').hide();
+	} else {
+	    $('#hideOnCountdown').show();
+	}
+
+    }
+
+
+
+    $(window).on('activate.bs.scrollspy', activateOnScroll)
+    activateOnScroll();
+
+});
