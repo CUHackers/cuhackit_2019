74996a25ad4501316c7e4dec92e17d7f5342323b
Angelo Carrabba <acarrabba6@gmail.com>
2018-01-26 18:10:48 -0500

    updated everything to work with scss

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # CU Hackathon Splash-Page
-### Sorry for formatting/other weirdness
-  
-  Commit changes often, we have version control for bad additions, but no
-  way to recoup lost work if it isn't saved.
 
-  For things which need to be worked on regarding the website, look under
-  "issues" in the navigation bar of this GitHub page. 
+## Installation
 
-  For things which need to be done, make an issue if not there
+You are going to need to have sass installed
+
+```bash
+$ gem install sass
+```
--- js/slowScrollSpy.js
+++ js/slowScrollSpy.js
@@ -1,11 +1,24 @@
+var scroll_offset=0;
+var spy_offset=88;
+
+
 $(document).ready(function(){
-    $('body').scrollspy({target: "nav", offset: 100});
+
+    $('body').scrollspy({target: "nav", offset: spy_offset});
+    /*
     $("nav a").on('click', function(event) {
+	event.preventDefault();
+	$($(this).attr('href'))[0].scrollIntoView();
+	scrollBy(0, -scroll_offset);
+    });*/
+
+
+    $("nav a").on('click', function(event) {
+	event.preventDefault();
 	if (this.hash !== "") {
-	    event.preventDefault();
 	    var hash = this.hash;
 	    $('html, body').animate({
-		scrollTop: $(hash).offset().top
+		scrollTop: $(hash).offset().top + -scroll_offset
 	    }, 800, function(){
 		window.location.hash = hash;
 	    });
